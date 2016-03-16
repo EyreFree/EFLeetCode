2762b854c580f5fa171b8ac49841bc8fde40b97c
EyreFree <EyreFree@163.com>
2016-03-16 17:23:11 +0800

    Initial Commit

---
+++ leetcode-swift-cpp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetcode-swift-cpp
+//
+//  Created by Venpoo on 16/3/16.
+//  Copyright © 2016年 eyrefree. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
