eaa461f1be29fe59e56e4f6cc015189b66538c96
EyreFree <eyrefree@163.com>
2016-04-05 09:16:39 +0800

    230. Kth Smallest Element in a BST

---
+++ leetcode-swift-cpp/230. Kth Smallest Element in a BST.swift
@@ -0,0 +1,43 @@
+//
+//  230. Kth Smallest Element in a BST.swift
+//  leetcode-swift-cpp
+//
+//  Created by MBP on 16/4/5.
+//  Copyright © 2016年 eyrefree. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution230KthSmallestElementinaBST {
+    var ans = 0
+
+    func kthSmallest(root: TreeNode?, _ k: Int) -> Int {
+        helper(root, k: k)
+        return ans
+    }
+
+    func helper(root: TreeNode?, k: Int) -> Int {
+        if root == nil {
+            return 0
+        }
+        let leftCount = helper(root!.left, k: k)
+        let rightCount = helper(root!.right, k: k - leftCount - 1)
+        if k == leftCount + 1 {
+            ans = root!.val
+        }
+        return leftCount + rightCount + 1
+    }
+}
