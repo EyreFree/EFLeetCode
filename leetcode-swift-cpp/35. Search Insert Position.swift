954292929097ad763f8d8fbb9c03a6665051ba2c
EyreFree <eyrefree@163.com>
2016-04-06 10:33:09 +0800

    35. Search Insert Position

---
+++ leetcode-swift-cpp/35. Search Insert Position.swift
@@ -0,0 +1,22 @@
+//
+//  35. Search Insert Position.swift
+//  leetcode-swift-cpp
+//
+//  Created by MBP on 16/4/6.
+//  Copyright © 2016年 eyrefree. All rights reserved.
+//
+
+import Foundation
+
+class Solution35SearchInsertPosition {
+    func searchInsert(nums: [Int], _ target: Int) -> Int {
+        for (index, value) in nums.enumerate() {
+            if value == target {
+                return index
+            } else if target < value {
+                return index
+            }
+        }
+        return nums.count
+    }
+}
