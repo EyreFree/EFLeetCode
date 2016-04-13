e9a5364a3c36ea409cd277a63759ae799c59a7c0
EyreFree <eyrefree@163.com>
2016-04-13 09:16:03 +0800

    231. Power of Two

---
+++ leetcode-swift-cpp/231. Power of Two.swift
@@ -0,0 +1,20 @@
+//
+//  231. Power of Two.swift
+//  leetcode-swift-cpp
+//
+//  Created by MBP on 16/4/13.
+//  Copyright © 2016年 eyrefree. All rights reserved.
+//
+
+import Foundation
+
+class Solution231PowerofTwo {
+    func isPowerOfTwo(n: Int) -> Bool {
+        if 0 < n {
+            if 0 == (n & (n - 1)) {
+                return true
+            }
+        }
+        return false
+    }
+}
