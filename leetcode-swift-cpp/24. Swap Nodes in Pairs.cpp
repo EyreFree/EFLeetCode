0d628d230fccd6860786cb8a3b76ee1073dcaef8
eyrefree <eyrefree@163.com>
2016-05-01 22:41:17 +0800

    24. Swap Nodes in Pairs

---
+++ leetcode-swift-cpp/24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,30 @@
+//
+//  24.cpp
+//  leetcode-swift-cpp
+//
+//  Created by EyreFree on 16/5/1.
+//  Copyright © 2016年 eyrefree. All rights reserved.
+//
+
+#include <stdio.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution24SwapNodesinPairs {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head && head->next) {
+            ListNode* p1 = head->next;
+            head->next = swapPairs(head->next->next);
+            p1->next = head;
+            head = p1;
+        }
+        return head;
+    }
+};
