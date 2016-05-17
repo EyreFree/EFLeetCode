01a677e131da3e2b7bf92c7dd87458d307101eaa
EyreFree <eyrefree@163.com>
2016-05-17 09:17:30 +0800

    110. Balanced Binary Tree

---
+++ leetcode-swift-cpp/110. Balanced Binary Tree.swift
@@ -0,0 +1,46 @@
+//
+//  110. Balanced Binary Tree.swift
+//  leetcode-swift-cpp
+//
+//  Created by MBP on 16/5/17.
+//  Copyright © 2016年 eyrefree. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution110BalancedBinaryTree {
+    func isBalanced(root: TreeNode?) -> Bool {
+        return checkBalance(root, height: 0).0
+    }
+
+    func checkBalance(root: TreeNode?, height: Int) -> (Bool, Int) {
+        if let tryRoot = root {
+            let leftResult = checkBalance(tryRoot.left, height: 0)
+            if !leftResult.0 {
+                return (false, height)
+            }
+            let rightResult = checkBalance(tryRoot.right, height: 0)
+            if !rightResult.0 {
+                return (false, height)
+            }
+            if abs(leftResult.1 - rightResult.1) > 1 {
+                return (false, height)
+            }
+            return (true, max(leftResult.1, rightResult.1) + 1)
+        }
+        return (true, 0)
+    }
+}
